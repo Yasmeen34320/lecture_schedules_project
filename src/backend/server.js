b4edaf6fe17d1f074cb62f3ed69a24f4751a8947
Yasmeen Alaa <y.alaa9.2001@gmail.com>
2023-12-28 20:25:57 +0200

    final2

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,57 +1,47 @@
 
 
-var express = require('express');
-var server = express();
-var routes = require('./routes/routes');
-var mongoose = require('mongoose');
+
+ const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
+const routes = require('./routes/routes');
+
 const app = express();
-const router = express.Router();
+const PORT = 8000;
 
+// Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/project', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-  });
-
-// mongoose.connect('mongodb://localhost:27017/project', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//    })
-//     .then(() => {
-//        console.log('Connected to MongoDB');
-//     })
-//     .catch((error) => {
-//        console.error('Error connecting to MongoDB:', error);
-//        process.exit(1);
-//     });
-//connectToDB();
-server.use(cors());
-
-app.use(cors({
-    origin: 'http://localhost:3000', // or your React app's origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  }));
-server.use(express.json());
-server.use(routes);
-
-server.listen(8000,function check(error)
-{
-    if(error)
-    {
-        console.log("errorr")
-    }
-    else
-    {
-        console.log("startedddddd")
-    }
+.then(() => {
+console.log('Connected to MongoDB');
+})
+.catch((err) => {
+console.error('Error connecting to MongoDB:', err);
 });
-router.post('/restart', function(req, res) {
-  // add code here to restart the server
-  res.send('Server is restarting...');
- });
- 
- module.exports = router;
+
+// Enable CORS
+app.use(cors({
+origin: 'http://localhost:3000', // or your React app's origin
+methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+credentials: true,
+}));
+
+// Parse JSON requests
+app.use(express.json());
+
+// Use routes
+app.use(routes);
+
+// Start the server
+app.listen(PORT, (error) => {
+if (error) {
+console.log('Error starting the server:', error);
+} else {
+console.log(`Server started on port ${PORT}`);
+}
+});
+
+// Define a route to restart the server
+app.post('/restart', (req, res) => {
+// add code here to restart the server
+res.send('Server is restarting...');
+});
